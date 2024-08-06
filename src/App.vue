eee5b8a4a63660c8a2f60c78ae3e72609e58a03e
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-08-06 20:09:47 +0200

    movie app - make interactive ratings and dynamically assign css classes

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from "vue";
+import { StarIcon } from "@heroicons/vue/24/solid";
+import { items } from "./movies.json";
+const movies = ref(items);
+
+</script>
+
+<template>
+  <div class="app">
+    <div class="movie-list">
+      <div class="movie-item" v-for="movie in movies" :key="movie.id">
+        <div class="movie-item-image-wrapper">
+          <img :src="movie.image" class="movie-item-image" alt="" />
+        </div>
+
+        <div class="movie-item-content-wrapper">
+          <div class="movie-item-title-wrapper">
+            <h3 class="movie-item-title">{{ movie.name }}</h3>
+            <div class="movie-item-genres-wrapper">
+              <span
+                v-for="genre in movie.genres"
+                :key="`${movie.id}-${genre}`"
+                class="movie-item-genre-tag"
+                >{{ genre }}</span
+              >
+            </div>
+          </div>
+          <div class="movie-item-description-wrapper">
+            <p class="movie-item-description">{{ movie.description }}</p>
+          </div>
+          <div class="movie-item-rating-wrapper">
+            <span class="movie-item-rating-text">
+              Rating: ({{ movie.rating }}/5)
+            </span>
+            <button class="flex flex-wrap">
+              <StarIcon
+                  v-for="star in 5"
+                  :key="`star-${star}`"
+                  :class="star <= movie.rating ? 'movie-item-star-icon' : 'movie-gray-star-icon'"
+                  @click="movie.rating = star "
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
